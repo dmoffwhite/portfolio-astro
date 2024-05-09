c483e169b06631dde1b224eede95b8a83be2d4f7
Diego Machado <68936381+dmoffwhite@users.noreply.github.com>
2024-05-09 00:44:59 -0600

    Portafolio terminado - Se agregaron las secciones que faltaban, Tecnologías, Proyectos y el Footer. También se añadió la navegación entre las secciones y los botones del header ya funcionan.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+				"animation": {
+				  "pulse-slow": "pulse 6s infinite cubic-bezier(0.4, 0, 0.6, 1)"
+				}
+		},
 	},
 	plugins: [],
 	
